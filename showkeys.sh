d8a3424048ecdcab1ac8e9df35e5830a901e73c2
EarthNodeChile <nodebasewm@github.com>
2023-02-13 10:42:35 -0300

    updates to showkeys thanks to the amazing Johnny

--- showkeys.sh
+++ showkeys.sh
@@ -8,13 +8,9 @@ NC='\033[0m'
 if [ -f "/opt/aya/registration.json" ]; then
 OPERATOR_ADDRESS=$(cat /opt/aya/registration.json | jq -r '.operator_address')
 OPERATOR_HEX=$(ayad keys parse $OPERATOR_ADDRESS --output json | jq -r '.bytes')
-
 VALIDATOR_ADDRESS=$(cat /opt/aya/registration.json | jq -r '.validator_address')
 VALIDATOR_HEX=$(ayad keys parse $VALIDATOR_ADDRESS --output json | jq -r '.bytes')
-
 MONIKER=$(cat /opt/aya/registration.json | jq -r '.moniker')
-
-SIZE=${#MONIKER}
 echo -e "${CYAN}$MONIKER${NC}"
 echo ""
 echo -e "${CYAN}OPERATOR${NC}"
@@ -25,7 +21,17 @@ echo -e "${CYAN}VALIDATOR${NC}"
 echo -e "  ${GREEN}$VALIDATOR_HEX${NC}"
 echo -e "  ${GREEN}$(ayad keys parse $VALIDATOR_HEX --output json | jq -r '.formats[4]')${NC}"
 echo -e "  ${GREEN}$(ayad keys parse $VALIDATOR_HEX --output json | jq -r '.formats[5]')${NC}"
-else
+else if [ -f "/opt/aya/sentry.json" ]; then
+VALIDATOR_ADDRESS=$(cat /opt/aya/sentry.json | jq -r '.validator_address')
+VALIDATOR_HEX=$(ayad keys parse $VALIDATOR_ADDRESS --output json | jq -r '.bytes')
+MONIKER=$(cat /opt/aya/sentry.json | jq -r '.moniker')
+echo -e "${CYAN}$MONIKER${NC}"
+echo ""
+echo -e "${CYAN}VALIDATOR${NC}"
+echo -e "  ${GREEN}$VALIDATOR_HEX${NC}"
+echo -e "  ${GREEN}$(ayad keys parse $VALIDATOR_HEX --output json | jq -r '.formats[4]')${NC}"
+echo -e "  ${GREEN}$(ayad keys parse $VALIDATOR_HEX --output json | jq -r '.formats[5]')${NC}"
 echo -e "${CYAN}NODE ID${NC}"
 echo -e "  ${GREEN}$(ayad tendermint show-node-id --home /opt/aya)${NC}"
+fi
 fi
