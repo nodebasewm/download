925fa7f02262af4a4b9f0ac4b036b232dd9b2768
EarthNodeChile <nodebasewm@github.com>
2023-02-25 11:30:38 -0300

    updates to showkeys.sh

--- showkeys.sh
+++ showkeys.sh
@@ -11,23 +11,25 @@ OPERATOR_HEX=$(ayad keys parse $OPERATOR_ADDRESS --output json | jq -r '.bytes')
 VALIDATOR_ADDRESS=$(cat /opt/aya/registration.json | jq -r '.validator_address')
 VALIDATOR_HEX=$(ayad keys parse $VALIDATOR_ADDRESS --output json | jq -r '.bytes')
 MONIKER=$(cat /opt/aya/registration.json | jq -r '.moniker')
-echo -e "${CYAN}$MONIKER${NC}"
+echo -e "${CYAN}$MONIKER : THIS IS A VALIDATOR NODE{NC}"
 echo ""
 echo -e "${CYAN}OPERATOR${NC}"
 echo -e "  ${GREEN}$OPERATOR_HEX${NC}"
 echo -e "  ${GREEN}$(ayad keys parse $OPERATOR_HEX --output json | jq -r '.formats[0]')${NC}"
 echo -e "  ${GREEN}$(ayad keys parse $OPERATOR_HEX --output json | jq -r '.formats[2]')${NC}"
-echo -e "${CYAN}VALIDATOR${NC}"
+echo -e "${CYAN}CONSENSUS${NC}"
 echo -e "  ${GREEN}$VALIDATOR_HEX${NC}"
 echo -e "  ${GREEN}$(ayad keys parse $VALIDATOR_HEX --output json | jq -r '.formats[4]')${NC}"
 echo -e "  ${GREEN}$(ayad keys parse $VALIDATOR_HEX --output json | jq -r '.formats[5]')${NC}"
+echo -e "${CYAN}NODE ID${NC}"
+echo -e "  ${GREEN}$(ayad tendermint show-node-id --home /opt/aya)${NC}"
 else if [ -f "/opt/aya/sentry.json" ]; then
 VALIDATOR_ADDRESS=$(cat /opt/aya/sentry.json | jq -r '.validator_address')
 VALIDATOR_HEX=$(ayad keys parse $VALIDATOR_ADDRESS --output json | jq -r '.bytes')
 MONIKER=$(cat /opt/aya/sentry.json | jq -r '.moniker')
-echo -e "${CYAN}$MONIKER${NC}"
+echo -e "${CYAN}$MONIKER: THIS IS A RELAY NODE${NC}"
 echo ""
-echo -e "${CYAN}VALIDATOR${NC}"
+echo -e "${CYAN}CONSENSUS${NC}"
 echo -e "  ${GREEN}$VALIDATOR_HEX${NC}"
 echo -e "  ${GREEN}$(ayad keys parse $VALIDATOR_HEX --output json | jq -r '.formats[4]')${NC}"
 echo -e "  ${GREEN}$(ayad keys parse $VALIDATOR_HEX --output json | jq -r '.formats[5]')${NC}"
